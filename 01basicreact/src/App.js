4c576a9043dddac4448726438532458f7dec7ac7
Hricha Sharma <sharmahricha6@gmail.com>
2024-06-08 14:12:11 +0530

    New commits

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello | Hricha Sharma</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+    <h1>Hello | Hricha Sharma</h1>
+  )
+}
+
+export default App
