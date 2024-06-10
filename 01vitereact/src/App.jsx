b6891fd5e44af1670fe1b7ab9d3d18f2fd18e3b3
Hricha Sharma <sharmahricha6@gmail.com>
2024-06-10 13:19:22 +0530

    new commits

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
+  const username = "Hi"
 
   return (
-    <h1>Hello | Hricha Sharma</h1>
+    <>
+    <h1>Chai aur react {username} </h1>
+    <p>test para</p>
+    </>
   )
 }
 
